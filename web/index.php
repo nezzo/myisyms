610ec7e8fddba6f1a7cc3a3a4ffd5fe28e3736fe
nezzo <lestatx@yandex.ru>
2016-10-16 02:19:45 +0300

    15.10.2016

---
+++ web/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> 
+    <title>Официальный сайт ISYMS</title>
+    <link href="assets/fcb1e97c/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    <!-- Internet Explorer HTML5 enabling script: -->
+    <!--[if IE]-->
+    </head>
+    <body>
+        <header>
+            <div class="container-fluid">
+                <div class="header_block">
+                    <div class="col-md-4">
+                        <div class="left_header_block">
+                            <span>ISYMS |</span>
+                            <p>Веб-программирование</p>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="right_header_block">
+                            <nav>
+                            <a>
+                               Контакты 
+                            </a>
+                            <a>
+                              Соц. Сети  
+                            </a>
+                            <a>
+                                Поиск
+                            </a>
+                            </nav>
+                        </div>    
+                    </div>  
+                </div>    
+            </div>    
+        </header>
+        <main>
+            <div class="page_news">
+                
+            </div>  
+        </main>
+        <footer>
+            <div class="dev_code">
+                <p>Created by Artur Legusha</p>
+            </div>    
+        <script src="assets/abb195eb/jquery.min.js" type="text/javascript"></script>
+        </footer>
+    </body>
+</html>
