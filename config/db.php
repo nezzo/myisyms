af1412544f17a8b6acb6339a8e3b91b5f8e6a95d
Nezzo <lestatx@yandex.ru>
2017-06-12 23:15:21 +0300

    Create db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=lagfucow_isyms',
-    'username' => 'lagfucow_isyms',
-    'password' => '=@87N}[?-1]0',
+    'username' => 'root',
+    'password' => '1111111',
     'charset' => 'utf8',
 ];
